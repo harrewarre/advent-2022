8d3753e059d6654f8af84f245c4dea1bfaef5d07
Sander Harrewijnen <sharrewijnen@live.com>
2022-12-07 17:04:38 +0100

    Day 3 - Part 2

--- src/day_3/Program.cs
+++ src/day_3/Program.cs
@@ -1,4 +1,5 @@
-﻿var backpacks = File.ReadLines("input.txt")
+﻿// Part 1
+var backpacks = File.ReadLines("input.txt")
     .Select((backpack) =>
     {
         var itemCount = backpack.Length;
@@ -13,6 +14,31 @@
         return  new { Dupes = priorities, CompartmentOne = compartmentOne, CompartmentTwo = compartmentTwo };
     });
 
+var total = backpacks.Select(bp => bp.Dupes.Sum(d => d.Priority)).Sum();
+Console.WriteLine($"Total priority value {total}");
+
+// Part 2
+var groups = File.ReadLines("input.txt")
+    .Chunk(3)
+    .Select((group) => {
+        var first = group.First();
+        var second = group.Skip(1).Take(1).First();
+        var third = group.Last();
+
+        var firstIntersect = first.Intersect(second);
+        var secondIntersect = firstIntersect.Intersect(third);
+
+        return firstIntersect.Intersect(secondIntersect).First();
+    }).Select((item) => {
+        return new {
+            Item = item,
+            Priority = GetItemPriority(item)
+        };
+    });
+
+var totalGroupPriority = groups.Sum(g => g.Priority);
+Console.WriteLine($"Total priority among all groups is {totalGroupPriority}");
+
 int GetItemPriority(char item)
 {
     var isUpper = char.IsUpper(item);
@@ -24,6 +50,3 @@ int GetItemPriority(char item)
 
     return priority;
 }
-
-var total = backpacks.Select(bp => bp.Dupes.Sum(d => d.Priority)).Sum();
-Console.WriteLine($"Total priority value {total}");
